7c4bed26db5c6864b7ff2bd372b3e62d3ee814fe
Arend-Jan Tetteroo <aj@arendjantetteroo.nl>
2011-07-12 17:01:48 +0200

    Fix getting identifier based on Entity or ODM

--- lib/Doctrine/Common/DataFixtures/ReferenceRepository.php
+++ lib/Doctrine/Common/DataFixtures/ReferenceRepository.php
@@ -76,7 +76,11 @@ class ReferenceRepository
         // in case if reference is set after flush, store its identity
         $uow = $this->manager->getUnitOfWork();
         if ($uow->isInIdentityMap($reference)) {
-            $this->identities[$name] = $uow->getEntityIdentifier($reference);
+            if(method_exists($uow, 'getEntityIdentifier')){
+                $this->identities[$name] = $uow->getEntityIdentifier($reference);
+            }elseif(method_exists($uow, 'getDocumentIdentifier')){
+                $this->identities[$name] = $uow->getDocumentIdentifier($reference);            
+            }
         }
     }
 
