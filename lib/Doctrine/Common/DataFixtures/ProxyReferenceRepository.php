cbf9589d9c2270efb7319b59dbe70c2a8b804402
Anthon Pang <anthonp@nationalfibre.net>
2012-04-19 23:25:38 -0400

    methods to serialize/unserialize reference repository

---
+++ lib/Doctrine/Common/DataFixtures/ProxyReferenceRepository.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
+
+namespace Doctrine\Common\DataFixtures;
+
+use Doctrine\Common\DataFixtures\ReferenceRepository;
+
+/**
+ * Proxy reference repository
+ *
+ * @since Doctrine ORM 2.2
+ *
+ * @author Anthon Pang <anthonp@nationalfibre.net>
+ */
+class ProxyReferenceRepository extends ReferenceRepository
+{
+    /**
+     * Serialize reference repository
+     *
+     * @return string
+     */
+    public function serialize() {
+        $simpleReferences = array();
+        foreach ($this->getReferences() as $name => $reference) {
+            $className = str_replace('Proxies\\__CG__\\', '', get_class($reference));
+            $simpleReferences[$name] = array($className, $reference->getId());
+        }
+
+        $serializedData = json_encode(array(
+            'references' => $simpleReferences,
+            'identities' => $this->getIdentities(),
+        ));
+
+	return $serializedData;
+    }
+
+    /**
+     * Unserialize reference repository
+     *
+     * @param string $serializedData Serialized data
+     */
+    public function unserialize($serializedData) {
+        $repositoryData = json_decode($serializedData, true);
+
+        $references = $repositoryData['references'];
+        foreach ($references as $name => $proxyReference) {
+            $this->setReference($name, $this->getManager()->getReference(
+                $proxyReference[0], // entity class name
+                $proxyReference[1]  // id
+            ));
+        }
+
+        $identities = $repositoryData['identities'];
+        foreach ($identities as $name => $identity) {
+            $this->setReferenceIdentity($name, $identity);
+        }
+    }
+}
