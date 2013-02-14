916486a50902dba12318e4b620a844f74a20f599
Guilherme Blanco <guilhermeblanco@gmail.com>
2013-02-14 02:28:19 -0500

    Removed old version.

--- lib/Doctrine/Common/DataFixtures/Exception/CircularReferenceException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Doctrine\Common\DataFixtures\Exception;
-
-use Doctrine\Common\CommonException;
-
-class CircularReferenceException extends CommonException
-{
-}
--- tests/Doctrine/Tests/Common/DataFixtures/TestFixtures/NotAFixture.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace TestFixtures;
-
-class NotAFixture
-{
-}
