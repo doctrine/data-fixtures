54cc61a3f2f28b102391ba4499737647ec6f6199
Marco Pivetta <ocramius@gmail.com>
2014-12-12 19:46:48 +0100

    #164 - PHPCR purger should expect fixture loading to use the transactional of doctrine/phpcr-odm#582

---
+++ tests/Doctrine/Tests/Common/DataFixtures/Executor/PHPCRExecutorTest.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
+
+namespace Doctrine\Tests\Common\DataFixtures\Executor;
+
+use Doctrine\Common\DataFixtures\Executor\PHPCRExecutor;
+use Exception;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Tests for {@see \Doctrine\Common\DataFixtures\Executor\PHPCRExecutor}
+ *
+ * @author Marco Pivetta <ocramius@gmail.com>
+ *
+ * @covers \Doctrine\Common\DataFixtures\Executor\PHPCRExecutor
+ */
+class PHPCRExecutorTest extends PHPUnit_Framework_TestCase
+{
+    public function testExecuteSingleFixtureWithNoPurge()
+    {
+        $dm       = $this->getDocumentManager();
+        $executor = new PHPCRExecutor($dm);
+        $fixture  = $this->getMockFixture();
+
+        $fixture->expects($this->once())->method('load')->with($dm);
+        $dm
+            ->expects($this->once())
+            ->method('transactional')
+            ->with($this->isType('callable'))
+            ->will($this->returnCallback(function ($callback) use ($dm) {
+                return $callback($dm);
+            }));
+
+        $executor->execute(array($fixture), true);
+    }
+
+    /**
+     * @return \Doctrine\Common\DataFixtures\Purger\PHPCRPurger|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getPurger()
+    {
+        return $this->getMock('Doctrine\Common\DataFixtures\Purger\PHPCRPurger', array(), array(), '', false);
+    }
+
+    /**
+     * @return \Doctrine\ODM\PHPCR\DocumentManager|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getDocumentManager()
+    {
+        return $this->getMock(
+            'Doctrine\ODM\PHPCR\DocumentManager',
+            array(
+                'transactional',
+                'flush',
+                'clear',
+            ),
+            array(),
+            '',
+            false
+        );
+    }
+
+    /**
+     * @return \Doctrine\Common\DataFixtures\FixtureInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getMockFixture()
+    {
+        return $this->getMock('Doctrine\Common\DataFixtures\FixtureInterface', array(), array(), '', false);
+    }
+}
