559908741345a8de61801a895d25c69856fdd099
Jonas Hoppe <j.hoppe@nexus-united.com>
2019-12-13 16:42:37 +0100

    Doctrine\Common\Persistence\ObjectManager is deprecated but you cant use the new Doctrine\Persistence\ObjectManager because of TypeHinting.

--- lib/Doctrine/Common/DataFixtures/FixtureInterface.php
+++ lib/Doctrine/Common/DataFixtures/FixtureInterface.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace Doctrine\Common\DataFixtures;
 
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\Persistence\ObjectManager as DeprecatedManager;
+use Doctrine\Persistence\ObjectManager;
 
 /**
  * Interface contract for fixture classes to implement.
@@ -13,6 +14,7 @@ interface FixtureInterface
 {
     /**
      * Load data fixtures with the passed EntityManager
+     * @param ObjectManager|DeprecatedManager $manager
      */
-    public function load(ObjectManager $manager);
+    public function load($manager);
 }
