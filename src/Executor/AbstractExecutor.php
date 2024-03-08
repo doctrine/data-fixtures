4b840ac1721309e8d0c2c212396089c9b5786118
Alexander M. Turek <me@derrabus.de>
2024-03-08 21:01:07 +0100

    Remove callable loggers (#471)

--- src/Executor/AbstractExecutor.php
+++ src/Executor/AbstractExecutor.php
@@ -4,21 +4,17 @@ declare(strict_types=1);
 
 namespace Doctrine\Common\DataFixtures\Executor;
 
-use Closure;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\DataFixtures\Purger\PurgerInterface;
 use Doctrine\Common\DataFixtures\ReferenceRepository;
 use Doctrine\Common\DataFixtures\SharedFixtureInterface;
-use Doctrine\Deprecations\Deprecation;
 use Doctrine\Persistence\ObjectManager;
 use Exception;
-use Psr\Log\AbstractLogger;
-use Psr\Log\LoggerInterface;
-use TypeError;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 
 use function get_debug_type;
-use function is_callable;
 use function sprintf;
 
 /**
@@ -26,21 +22,16 @@ use function sprintf;
  *
  * @internal since 1.8.0
  */
-abstract class AbstractExecutor
+abstract class AbstractExecutor implements LoggerAwareInterface
 {
+    use LoggerAwareTrait;
+
     /**
      * Purger instance for purging database before loading data fixtures
      *
      * @var PurgerInterface|null
      */
     protected $purger;
-
-    /**
-     * Logger callback for logging messages when loading data fixtures
-     *
-     * @var (LoggerInterface&callable)|null
-     */
-    protected $logger;
 
     /**
      * Fixture reference repository
@@ -82,110 +73,6 @@ abstract class AbstractExecutor
     }
 
     /**
-     * Set the logger callable to execute with the log() method.
-     *
-     * @param LoggerInterface|callable|null $logger
-     *
-     * @return void
-     */
-    public function setLogger($logger)
-    {
-        if ($logger instanceof LoggerInterface) {
-            $logger = new class ($logger) extends AbstractLogger
-            {
-                private LoggerInterface $logger;
-
-                public function __construct(LoggerInterface $logger)
-                {
-                    $this->logger = $logger;
-                }
-
-                /** @inheritDoc */
-                public function log($level, $message, array $context = []): void
-                {
-                    $this->logger->log($level, $message, $context);
-                }
-
-                public function __invoke(string $message): void
-                {
-                    Deprecation::trigger(
-                        'doctrine/data-fixtures',
-                        'https://github.com/doctrine/data-fixtures/pull/462',
-                        'Invoking the logger is deprecated, call %s methods instead',
-                        LoggerInterface::class,
-                    );
-
-                    $this->logger->debug($message);
-                }
-            };
-        } elseif (is_callable($logger)) {
-            Deprecation::trigger(
-                'doctrine/data-fixtures',
-                'https://github.com/doctrine/data-fixtures/pull/462',
-                'Passing a callable to %s() is deprecated, pass an instance of %s instead',
-                __METHOD__,
-                LoggerInterface::class,
-            );
-
-            $logger = new class ($logger) extends AbstractLogger
-            {
-                private Closure $logger;
-
-                public function __construct(callable $logger)
-                {
-                    $this->logger = Closure::fromCallable($logger);
-                }
-
-                /** @inheritDoc */
-                public function log($level, $message, array $context = []): void
-                {
-                    ($this->logger)($message);
-                }
-
-                public function __invoke(string $message): void
-                {
-                    Deprecation::trigger(
-                        'doctrine/data-fixtures',
-                        'https://github.com/doctrine/data-fixtures/pull/462',
-                        'Invoking the logger is deprecated, call %s methods instead',
-                        LoggerInterface::class,
-                    );
-
-                    ($this->logger)($message);
-                }
-            };
-        } elseif ($logger !== null) {
-            throw new TypeError(sprintf(
-                '%s(): Parameter $logger is expected to be an instance of %s, %s given.',
-                __METHOD__,
-                LoggerInterface::class,
-                get_debug_type($logger),
-            ));
-        }
-
-        $this->logger = $logger;
-    }
-
-    /**
-     * Logs a message using the logger.
-     *
-     * @deprecated without replacement
-     *
-     * @return void
-     */
-    public function log(string $message)
-    {
-        Deprecation::triggerIfCalledFromOutside(
-            'doctrine/data-fixtures',
-            'https://github.com/doctrine/data-fixtures/pull/462',
-            'Method %s() is deprecated',
-            __METHOD__,
-        );
-
-        $this->logger->debug($message);
-    }
-
-    /**
      * Load a fixture with the given persistence manager.
      *
      * @return void
@@ -198,7 +85,7 @@ abstract class AbstractExecutor
                 $prefix = sprintf('[%d] ', $fixture->getOrder());
             }
 
-            $this->log('loading ' . $prefix . get_debug_type($fixture));
+            $this->logger->debug('loading ' . $prefix . get_debug_type($fixture));
         }
 
         // additionally pass the instance of reference repository to shared fixtures
@@ -226,9 +113,7 @@ abstract class AbstractExecutor
             );
         }
 
-        if ($this->logger) {
-            $this->log('purging database');
-        }
+        $this->logger?->debug('purging database');
 
         $this->purger->purge();
     }
