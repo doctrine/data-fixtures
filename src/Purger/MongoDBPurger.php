2f8cc42d0ed4cade825ce1226f75e76f6125798f
Jérôme Tamarelle <jerome@tamarelle.net>
2025-07-07 12:21:24 +0200

    Add purge mode to MongoDB Purger

--- src/Purger/MongoDBPurger.php
+++ src/Purger/MongoDBPurger.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Doctrine\Common\DataFixtures\Purger;
 
 use Doctrine\ODM\MongoDB\DocumentManager;
+use LogicException;
 
 /**
  * Class responsible for purging databases of data before reloading data fixtures.
@@ -12,12 +13,45 @@ use Doctrine\ODM\MongoDB\DocumentManager;
 final class MongoDBPurger implements MongoDBPurgerInterface
 {
     /**
+     * Purge the collections using deleteMany(). Don't create them.
+     */
+    public const PURGE_MODE_DELETE = 1;
+
+    /**
+     * Drop the collections when purging, then recreate them.
+     */
+    public const PURGE_MODE_DROP = 2;
+
+    /** @var self::PURGE_MODE_* $mode */
+    private int $purgeMode = self::PURGE_MODE_DELETE;
+
+    /**
      * Construct new purger instance.
      *
      * @param DocumentManager|null $dm DocumentManager instance used for persistence.
      */
     public function __construct(private DocumentManager|null $dm = null)
     {
+    }
+
+    /**
+     * If the purge should be done through collection drop() or deleteMany() statements
+     *
+     * @param self::PURGE_MODE_* $mode
+     */
+    public function setPurgeMode(int $mode): void
+    {
+        $this->purgeMode = $mode;
+    }
+
+    /**
+     * Get the purge mode
+     *
+     * @return self::PURGE_MODE_*
+     */
+    public function getPurgeMode(): int
+    {
+        return $this->purgeMode;
     }
 
     /**
@@ -38,8 +72,29 @@ final class MongoDBPurger implements MongoDBPurgerInterface
 
     public function purge(): void
     {
-        $metadatas = $this->dm->getMetadataFactory()->getAllMetadata();
-        foreach ($metadatas as $metadata) {
+        match ($this->purgeMode) {
+            self::PURGE_MODE_DELETE => $this->purgeWithDelete(),
+            self::PURGE_MODE_DROP => $this->purgeWithDrop(),
+            default => throw new LogicException('Invalid purge mode specified.'),
+        };
+    }
+
+    private function purgeWithDelete(): void
+    {
+        $allMetadata = $this->dm->getMetadataFactory()->getAllMetadata();
+        foreach ($allMetadata as $metadata) {
+            if ($metadata->isMappedSuperclass) {
+                continue;
+            }
+
+            $this->dm->getDocumentCollection($metadata->name)->deleteMany([]);
+        }
+    }
+
+    private function purgeWithDrop(): void
+    {
+        $allMetadata = $this->dm->getMetadataFactory()->getAllMetadata();
+        foreach ($allMetadata as $metadata) {
             if ($metadata->isMappedSuperclass) {
                 continue;
             }
@@ -47,6 +102,11 @@ final class MongoDBPurger implements MongoDBPurgerInterface
             $this->dm->getDocumentCollection($metadata->name)->drop();
         }
 
-        $this->dm->getSchemaManager()->ensureIndexes();
+        $schemaManager = $this->dm->getSchemaManager();
+        $schemaManager->createCollections();
+        $schemaManager->ensureIndexes();
+        if (method_exists($schemaManager, 'createSearchIndexes')) {
+            $schemaManager->createSearchIndexes();
+        }
     }
 }
--- tests/Common/DataFixtures/Purger/MongoDBPurgerTest.php
+++ tests/Common/DataFixtures/Purger/MongoDBPurgerTest.php
@@ -49,6 +49,37 @@ class MongoDBPurgerTest extends BaseTestCase
         return new MongoDBPurger($this->getDocumentManager());
     }
 
+    public function testPurgeWithDelete(): void
+    {
+        $purger = $this->getPurger();
+        $dm     = $purger->getObjectManager();
+        $purger->setPurgeMode(MongoDBPurger::PURGE_MODE_DELETE);
+
+        $collection = $dm->getDocumentCollection(self::TEST_DOCUMENT_ROLE);
+        $collection->drop();
+
+        $this->assertIndexCount(0, $collection);
+
+        $purger->purge();
+
+        // Collection isn't created yet, so no indices should be present
+        $this->assertIndexCount(0, $collection);
+
+        $role = new Role();
+        $role->setName('role');
+        $dm->persist($role);
+        $dm->flush();
+
+        // Only the _id_ index should be present after the document is created
+        $this->assertIndexCount(1, $collection);
+
+        $purger->purge();
+
+        // After purging, the collection should still exist, but the document should be deleted
+        $this->assertIndexCount(1, $collection);
+        $this->assertSame(0, $collection->countDocuments());
+    }
+
     public function testPurgeKeepsIndices(): void
     {
         $purger = $this->getPurger();
